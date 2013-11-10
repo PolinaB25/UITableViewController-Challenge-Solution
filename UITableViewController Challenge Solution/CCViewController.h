dc6935a4b0e4840076d93effe5fa8e5868436514
Polina Bates <Polina@Polinas-MacBook-Pro.local>
2013-11-10 15:55:14 -0600

    Initial Commit

---
+++ UITableViewController Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Polina Boldyreva on 11/10/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
